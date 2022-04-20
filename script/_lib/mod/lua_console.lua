abdf4500f70d44fe7d7f224c8e17d05f583f7015
vandy <chadvandewettering@gmail.com>
2022-04-20 18:57:13 -0400

    rework up to last release

--- script/_lib/mod/lua_console.lua
+++ script/_lib/mod/lua_console.lua
@@ -1,8 +1,10 @@
 --- TODO vscode-lua'ify WH3
---- TODO add in "enter" support
---- TODO paste functionality (???)
+--- TODO add in "enter" support (not likely)
 --- TODO resizable
 --- TODO put an info button
+
+--- TODO MP support
+--- TODO an "inspector" feature, you press it and it tells you information of the currently selected thing (selected character etc)
 
 out("qa_console.lua loaded");
 
@@ -12,16 +14,19 @@ local lua_console = {
 	_layout_path = "ui/dev_ui/!qa_console",
 	_shortcut_key = "script_shift_F3", --- TODO expose to MCT
 
-	_uic = nil, --- TODO the UIC itself
+	_uic = nil,
 
 	---@type number The current line selected.
 	_current_line = 0,
 
 	---@type number The number of line components currently created
 	_num_lines = 0,
-	_lines = {},
-	_max_lines = 90,
-	_max_characters = 100, --- TODO demo
+
+	---@type number The last visible line, for when there are more lines created than visible. Only executes code up to this line.
+	_last_visible_line = 0,
+
+	---@type number The maximum number of lines. Currently effectively disabled, I don't think it's needed.
+	_max_lines = 9000,
 }
 
 function lua_console:create()
@@ -29,8 +34,15 @@ function lua_console:create()
 	self._uic:SetVisible(false);
 	self._uic:AddScriptEventReporter()
 
-	local entry_box = find_uicomponent(self._uic, "entry_box")
-	entry_box:SetStateText("")
+	-- local w,h = self._uic:Dimensions()
+
+	-- local listview = find_uicomponent(self._uic, "listview")
+	-- listview:Resize(w, 200)
+
+	local listview = find_uicomponent(self._uic, "listview")
+	local vslider = find_uicomponent(listview, "vslider")
+
+	vslider:Resize(vslider:Width(), listview:Height()-50)
 
 	local text_popup = core:get_or_create_component("text_popup", "ui/common ui/tooltip_text_only", self._uic)
 
@@ -38,8 +50,64 @@ function lua_console:create()
 	text_popup:SetCanResizeWidth(true) text_popup:SetCanResizeHeight(true)
 	text_popup:Resize(w + 50, h * 0.8)
 	text_popup:SetDockingPoint(8)
-	text_popup:SetDockOffset(0, 15)
+	text_popup:SetDockOffset(0, 60)
 	text_popup:SetVisible(false)
+
+	local bottom_bar = find_uicomponent(self._uic, "bottom_bar_container")
+	local container = find_uicomponent(bottom_bar, "button_add_remove_container")
+
+	local add_icon = find_uicomponent(container, "button_add_line")
+	local remove_icon = find_uicomponent(container, "button_remove_line")
+
+	add_icon:Destroy()
+	remove_icon:Destroy()
+
+	local button_add_line = core:get_or_create_component("button_add_line", "ui/templates/square_small_button", container)
+	local button_remove_line = core:get_or_create_component("button_remove_line", "ui/templates/square_small_button", container)
+
+	local button_copy = core:get_or_create_component("button_copy", "ui/templates/square_small_button", container)
+	local button_paste = core:get_or_create_component("button_paste", "ui/templates/square_small_button", container)
+
+	find_uicomponent(button_copy, "icon"):SetVisible(false)
+	find_uicomponent(button_paste, "icon"):SetVisible(false)
+	find_uicomponent(button_add_line, "icon"):SetVisible(false)
+	find_uicomponent(button_remove_line, "icon"):SetVisible(false)
+
+	-- find_uicomponent(button_copy, "icon"):SetImagePath(common.get_context_value("AddDefaultSkinPath(\"icon_encyclopedia.png\")"))
+	-- find_uicomponent(button_paste, "icon"):SetImagePath(common.get_context_value("AddDefaultSkinPath(\"icon_rename.png\")"))
+	button_copy:SetImagePath("ui/skins/default/icon_encyclopedia.png")
+	button_paste:SetImagePath("ui/skins/default/icon_rename.png")
+	button_add_line:SetImagePath("ui/skins/default/icon_plus_small.png")
+	button_remove_line:SetImagePath("ui/skins/default/icon_minus_small.png")
+
+	button_copy:SetTooltipText("Copy the Lua Console to your clipboard.\n[[col:red]]WARNING: There's currently some weird issues with this. If you're having trouble not copying, close and reopen the Lua Console a few times. Your code will remain.[[/col]]", true)
+	button_paste:SetTooltipText("Paste the contents of your clipboard to the Lua Console.\nBegins pasting on the first line after your existing code; if there aren't available lines, they will be created.", true)
+	button_add_line:SetTooltipText("Add a new line.", true)
+	button_remove_line:SetTooltipText("Remvoe the final line (text will be preserved, but not executed).", true)
+
+	button_copy:SetCanResizeWidth(true) button_copy:SetCanResizeHeight(true)
+	button_paste:SetCanResizeWidth(true) button_paste:SetCanResizeHeight(true)
+	button_add_line:SetCanResizeWidth(true) button_add_line:SetCanResizeHeight(true)
+	button_remove_line:SetCanResizeWidth(true) button_remove_line:SetCanResizeHeight(true)
+
+	local w,h = 36,36
+
+	button_copy:Resize(w, h)
+	button_paste:Resize(w, h)
+	button_add_line:Resize(w, h)
+	button_remove_line:Resize(w, h)
+
+	button_copy:SetDockingPoint(4)
+	button_copy:SetDockOffset(0, 0)
+	button_paste:SetDockingPoint(4)
+	button_paste:SetDockOffset(w+4, 0)
+	button_add_line:SetDockingPoint(6)
+	button_add_line:SetDockOffset(w+4, 0)
+	button_remove_line:SetDockingPoint(6)
+	button_remove_line:SetDockOffset(0, 0)
+
+	--- TEMP
+	button_copy:SetTooltipText("Copy the Lua Console to a local file.\n[[col:red]]There's currently a bug where you can't copy anything with ' or \" in it, making it functionally useless. For now, this prints the Lua Console to a file at Total War WARHAMMER III/lua_console_code.lua[[/col]]", true)
 
 	self:setup_text_input()
 	self:init_listeners()
@@ -50,61 +118,117 @@ function lua_console:swap_visibility()
 	if uic then
 		local b = not uic:Visible()
 		uic:SetVisible(b)
-		self:set_repeat_callback(b)
 
 		if b then self:set_current_line(1) end
 	end
 end
 
---- TODO create each text_input line (start with 8, create more as needed)
---- TODO create the listview
 function lua_console:setup_text_input()
-	local entry_box = find_uicomponent(self:get_uic(), "entry_box")
-
-	self._num_lines = 8
-
-	for i = 1, self._num_lines do
-		---@type UIComponent
-		out("Creating text_input_"..i)
-		local text_input = core:get_or_create_component("text_input_"..i, "ui/dev_ui/text_box", entry_box)
-		text_input:SetCanResizeHeight(true) text_input:SetCanResizeWidth(true)
-	
-		w,h = entry_box:Dimensions()
-		local hi = h/8
-		h = hi
-		local y = hi * (i-1)
-	
-		text_input:Resize(w, h)
-		text_input:SetDockingPoint(1)
-		text_input:SetDockOffset(0, y)
-
-		text_input:SetCanResizeHeight(false) text_input:SetCanResizeWidth(false)
-
-		local line_text = core:get_or_create_component("line_"..i, "ui/dev_ui/text", text_input)
-		line_text:SetDockingPoint(4)
-		line_text:SetDockOffset(-12, 0)
-		line_text:SetStateText(tostring(i)..":")
+	for i = 1, 8 do
+		self:create_text_line(i)
 	end
 
 	self:set_current_line(1)
 end
 
---- TODO if not a valid input then add a new one at the bottom
+--- TODO dynamically resize the text input so it's always 6px to the right of the :, so it doesn't look weird on different numbers
+function lua_console:create_text_line(i)
+	local listview = find_uicomponent(self:get_uic(), "listview")
+	local text_box = find_uicomponent(listview, "list_clip", "list_box")
+
+	if not i then i = self._num_lines + 1 end
+	if i >= self._max_lines then return end
+
+	local extant = find_uicomponent(text_box, "text_input_"..i)
+	if is_uicomponent(extant) then
+		if extant:Visible() then return end
+		extant:SetVisible(true)
+		self._last_visible_line = i
+		return
+	end
+
+	---@type UIComponent
+	out("Creating text_input_"..i)
+	local text_input = core:get_or_create_component("text_input_"..i, "ui/dev_ui/text_box", text_box)
+	text_input:SetCanResizeHeight(true) text_input:SetCanResizeWidth(true)
+
+	local w,h = listview:Dimensions()
+	local hi = h/8
+	h = hi
+	
+	local line_text = core:get_or_create_component("line_"..i, "ui/dev_ui/text", text_input)
+	local tw = line_text:Width()*1.5
+	line_text:Resize(tw, h)
+	line_text:SetDockingPoint(1)
+	line_text:SetDockOffset(-tw, 0)
+	line_text:SetStateText(tostring(i)..":")
+	
+	text_input:Resize(w-tw-20, h)
+	text_input:SetDockingPoint(4)
+	text_input:SetDockOffset(tw, 0)
+
+	text_input:SetCanResizeHeight(false) text_input:SetCanResizeWidth(false)
+
+	self._num_lines = i
+	self:show_line(i)
+
+	text_box:Layout()
+
+	return text_input
+end
+
+--- Hide the final line in the block. Keeps the line created, so it can be re-added and 
+---@param i any
+function lua_console:remove_line(i)
+	local i
+	if not i then i = self._last_visible_line end
+
+	if i <= 5 then return end
+
+	local input = find_uicomponent(self._uic, "listview", "list_clip", "list_box")
+	local line = find_uicomponent(input, "text_input_"..i)
+	if is_uicomponent(line) then
+		line:SetVisible(false)
+		self._last_visible_line = i - 1
+		return true
+	end
+
+	return false
+end
+
+function lua_console:show_line(i)
+	if not i then i = self._last_visible_line + 1 end
+
+	local input = find_uicomponent(self._uic, "listview", "list_clip", "list_box")
+	local line = find_uicomponent(input, "text_input_"..i)
+	if is_uicomponent(line) then
+		line:SetVisible(true)
+		self._last_visible_line = i
+		return true
+	end
+
+	return false
+end
+
 function lua_console:set_current_line(i)
 	if not is_number(i) then
 		--- errmsg
 		return false
 	end
+	
+	local input = self:get_text_input(i)
+	if not input then
+		input = self:create_text_line(i)
+	end
 
 	self._current_line = i
-	local input = self:get_text_input(i)
 	input:SimulateLClick()
 end
 
 ---@return UIComponent
 function lua_console:get_text_input(i)
 	if not i then i = 1 end
-	local entry_box = find_uicomponent(self:get_uic(), "entry_box")
+	local entry_box = find_uicomponent(self:get_uic(), "listview", "list_clip", "list_box")
 	local text_input = find_uicomponent(entry_box, "text_input_"..i)
 
 	if not text_input then
@@ -116,29 +240,28 @@ function lua_console:get_text_input(i)
 	return text_input
 end
 
---- TODO make a new line when we're out of room!
-function lua_console:set_repeat_callback(b)
-	---@type timer_manager
-	local tm = core:get_static_object("timer_manager")
-	if b == true then
-		-- hook up the repeat callback
-		out("Setting up repeat callback")
-		tm:repeat_real_callback(function()
-			local line = self:get_text_input(self._current_line)
-			local txt = line:GetStateText()
-			local w = line:Width() - 20
-			local tw = line:TextDimensionsForText(txt)
+-- function lua_console:set_repeat_callback(b)
+-- 	---@type timer_manager
+-- 	local tm = core:get_static_object("timer_manager")
+-- 	if b == true then
+-- 		-- hook up the repeat callback
+-- 		out("Setting up repeat callback")
+-- 		tm:repeat_real_callback(function()
+-- 			local line = self:get_text_input(self._current_line)
+-- 			local txt = line:GetStateText()
+-- 			local w = line:Width() - 20
+-- 			local tw = line:TextDimensionsForText(txt)
 
-			if tw >= w then
-				self:set_current_line(self._current_line + 1)
-			end
-		end, 10, "check_lua_console")
-	else
-		-- kill it
-		out("Removing repeat callback")
-		tm:remove_real_callback("check_lua_console")
-	end
-end
+-- 			if tw >= w then
+-- 				self:set_current_line(self._current_line + 1)
+-- 			end
+-- 		end, 10, "check_lua_console")
+-- 	else
+-- 		-- kill it
+-- 		out("Removing repeat callback")
+-- 		tm:remove_real_callback("check_lua_console")
+-- 	end
+-- end
 
 --- TODO print out to a logfile as well.
 
@@ -151,8 +274,9 @@ function lua_console:print(text)
 	popup:SetVisible(true)
 	local t = text_uic:GetStateText()
 
-	--- TODO fix newlines on first line
-	text_uic:SetStateText(t .. "\n" .. text)
+	if t ~= "" then t = t .. "\n" end
+
+	text_uic:SetStateText(t .. text)
 end
 
 function lua_console:printf(text, ...)
@@ -173,21 +297,111 @@ function lua_console:get_text_popup()
 	return popup, text
 end
 
-function lua_console:get_text()
-	out("Getting text!")
+function lua_console:get_text(visible_only)
+	ModLog("Getting text!")
 	local num_lines = self._num_lines
-	local txt = {}
+	local str = ""
+
+	if visible_only then num_lines = self._last_visible_line end
 
 	for i = 1, num_lines do
 		local line = self:get_text_input(i)
 		local t = line:GetStateText()
 		if t ~= "" then
-			txt[#txt+1] = t
+			if i ~= 1 then str = str .. "\n" end
+			str = str .. t
 		end
 	end
 
-	local str = table.concat(txt, "\n")
 	return str
+end
+
+function lua_console:set_visible_up_to_line(i)
+	if not i then i = self._num_lines end
+	if i < self._last_visible_line then i = self._last_visible_line end
+
+	for j = 1, i do
+		self:show_line(j)
+	end
+end
+
+--- Grab the final line with any user input within.
+---@param visible_only boolean If we should check all lines, or just currently visible ones.
+---@return integer
+function lua_console:get_last_used_line(visible_only)
+	local start = visible_only and self._last_visible_line or self._num_lines
+	local last_line
+
+	ModLog("Looping from " .. start .. " to 1.")
+	for i = start, 1, -1 do
+		ModLog("Checking line #"..i)
+		local line = self:get_text_input(i)
+		if line then
+			local text = line:GetStateText()
+			
+			if text ~= "" then
+				break
+			end
+			
+			last_line = i
+		end
+	end
+
+	return last_line
+end
+
+function lua_console:copy_to_clipboard()
+	local text = self:get_text(true)
+	ModLog("Copying to Clipboard: \n" .. text)
+	-- common.call_context_command("CopyStringToClipboard(\""..text.."\")")
+
+	local file = io.open("lua_console_code.lua", "w+")
+	file:write(text)
+	file:close()
+end
+
+local function string_split(str, delimiter)
+	local result = { }
+	local from  = 1
+	local delim_from, delim_to = string.find( str, delimiter, from  )
+	while delim_from do
+		table.insert( result, string.sub( str, from , delim_from-1 ) )
+		from  = delim_to + 1
+		delim_from, delim_to = string.find( str, delimiter, from  )
+	end
+	table.insert( result, string.sub( str, from  ) )
+	return result
+end
+
+--- TODO issue if you have 10 lines written, hide the last 2, and then try to paste - it will paste at 11, but will still have 9-10 hidden.
+function lua_console:paste_from_clipboard()
+	ModLog("Pasting from clipboard!")
+
+	--- get the last line that is untouched
+	local last_line = self:get_last_used_line()
+	if not last_line then
+		self:create_text_line()
+		last_line = self._num_lines
+	end
+
+	self:set_visible_up_to_line(last_line)
+
+	ModLog("Pasting from clipboard - last line with stuff is " .. last_line)
+
+	-- starting from the last untouched line, loop and paste.
+	local clipboard = common.get_context_value("PasteStringFromClipboard")
+	local tab = string_split(clipboard, "\n")
+	
+	ModLog("Clipboard text is " .. clipboard)
+	ModLog("Number of lines: "..#tab)
+
+	for i = 1, #tab do
+		local this_line = last_line + (i-1)
+		ModLog("Pasting on line " .. this_line)
+		self:create_text_line(this_line)
+		local line = self:get_text_input(this_line)
+		line:SetStateText(tab[i])
+	end
 end
 
 function lua_console:clear_popup()
@@ -208,7 +422,7 @@ end
 function lua_console:execute()
 	out("Executing")
 	self:clear_popup()
-	local text = self:get_text()
+	local text = self:get_text(true)
 
 	out("Executing text: " .. text)
 
@@ -246,10 +460,9 @@ end
 
 function lua_console:init_listeners()
 	core:add_listener(
-		"qa_console_listener",
+		"lua_console_listener",
 		"ShortcutPressed",
 		function(context)
-			out("ShortcutPressed event occurred, context.string is " .. context.string);
 			return context.string == lua_console._shortcut_key
 		end,
 		function(context)
@@ -259,50 +472,44 @@ function lua_console:init_listeners()
 	);
 
 	core:add_listener(
-		"qa_console_lclickup",
-		"ComponentLClickUp", 
+		"lua_console_lclickup",
+		"ComponentLClickUp",
 		function(context)
-			local uic = UIComponent(context.component);
-			return uic:Id() == "button_run" and uicomponent_descended_from(uic, lua_console._name);
+			return uicomponent_descended_from(UIComponent(context.component), lua_console._name)
 		end,
-		function()
-			out("Executing")
-			local ok, err = pcall(function()
-			lua_console:execute();
-		end) if not ok then out(err) end
+		function(context)
+			local id = context.string
+
+			--- TODO I'm too old to do an if/else string like this, clean up pls
+			if id == "button_run" then
+				lua_console:execute()
+			elseif id == "button_close" then
+				lua_console:swap_visibility()
+			elseif id == "button_clear" then 
+				lua_console:clear_text()
+				lua_console:clear_popup()
+			elseif id == "button_add_line" then
+				if not lua_console:show_line() then
+					lua_console:create_text_line()
+				end
+			elseif id == "button_remove_line" then
+				lua_console:remove_line()
+			elseif id == "button_copy" then
+				local ok, err = pcall(function()
+					lua_console:copy_to_clipboard()
+				end) if not ok then ModLog(err) end
+			elseif id == "button_paste" then
+				local ok, err = pcall(function()
+					lua_console:paste_from_clipboard()
+				end) if not ok then ModLog(err) end
+			end
 		end,
 		true
-	);
+	)
+
 
 	core:add_listener(
-		"qa_console_lclickup_close",
-		"ComponentLClickUp", 
-		function(context)
-			local uic = UIComponent(context.component);
-			return uic:Id() == "button_close" and uicomponent_descended_from(uic, lua_console._name);
-		end,
-		function()
-			lua_console:get_uic():SetVisible(false);
-		end,
-		true
-	);
-
-	core:add_listener(
-		"qa_console_lclickup_clear",
-		"ComponentLClickUp", 
-		function(context)
-			local uic = UIComponent(context.component);
-			return uic:Id() == "button_clear" and uicomponent_descended_from(uic, lua_console._name);
-		end,
-		function()
-			lua_console:clear_text()
-			lua_console:clear_popup()
-		end,
-		true
-	);
-
-	core:add_listener(
-		"qa_console_moved",
+		"lua_console_moved",
 		"ComponentMoved",
 		function(context)
 			return context.string == lua_console._name
@@ -330,6 +537,9 @@ end
 function console_printf(t, ...)
 	lua_console:printf(t, ...)
 end
+
+c_print = console_print 
+c_printf = console_printf
 
 function t_get(t, i)
 	if not is_table(t) then return end
